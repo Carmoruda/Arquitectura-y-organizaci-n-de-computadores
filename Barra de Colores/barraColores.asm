2255ab3eb1db2ed61ec987722e250d6b5fbde9b0
Carmen <carmen.moreno.udaondo@gmail.com>
2023-10-14 09:42:08 +0200

    Color con el registro A, ya que C no hace falta

--- Barra de Colores/barraColores.asm
+++ Barra de Colores/barraColores.asm
@@ -7,24 +7,21 @@ BEGIN:
         LD SP, 0        ; Establecer el puntero de pila en la parte alta de la memoria
         LD HL, $5800    ; Primer cuadrado de la pantalla. 
 MAIN:
-        LD C, 0         ; Color inicial (negro).
+        LD A, 0         ; Color inicial (negro).
 PAINTLINE:
-        LD A, 8         ; i (Contador PAINTLINE).
-        LD B, 4         ; j (Contador PAINTCOLOR).
+        LD B, 4         ; Contador PAINTCOLOR.
 PAINTCOLOR:
-        LD (HL), C              ; Pintar cuadrado del color.
+        LD (HL), A              ; Pintar cuadrado del color.
         INC HL                  ; Siguiente cuadrado.
-        DJNZ PAINTCOLOR
+        DJNZ PAINTCOLOR         ; For (i = 4, i > 0, i--)
 CHANGECOLOR:
-        PUSH AF
-        LD A, C
         ADD 8                   ; Cambiar color.
         AND %00111000           ; Solo color paper.
-        LD C, A
+        PUSH AF
         LD A, L
-        CP $5B                  ; while (HL != $5B00)
+        CP $5B                  
         POP AF
-        JR NZ, PAINTLINE
+        JR NC, PAINTLINE        ; while (HL != $5B00)
 
 ENDOFCODE:            
         JR ENDOFCODE
