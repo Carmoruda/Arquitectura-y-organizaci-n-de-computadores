d9c5fc6f7532fee30c6b430f5e46c6bbf98cc389
Carmen <carmen.moreno.udaondo@gmail.com>
2023-10-16 10:25:15 +0200

    Language to english and Readme

--- Barra de Colores/barraColores.asm
+++ Barra de Colores/barraColores.asm
@@ -1,23 +1,23 @@
         DEVICE ZXSPECTRUM48
 	SLDOPT COMMENT WPMEM, LOGPOINT, ASSETION
-        ORG $8000               ; Programa ubicado a partir de $8000 = 32768
+        ORG $8000               ; Program located at $8000 = 32768.
 
 BEGIN:          
-        DI              ; Deshabilitar interrupciones
-        LD SP, 0        ; Establecer el puntero de pila en la parte alta de la memoria
-        LD HL, $5800    ; Primer cuadrado de la pantalla. 
+        DI              ; Disable interruptions.
+        LD SP, 0        ; Set the stack pointer to the top of memory.
+        LD HL, $5800    ; First square of the screen. 
 MAIN:
-        LD C, 0         ; Color inicial (negro).
+        LD C, 0         ; Initial color (black).
 PAINTLINE:
-        LD B, 4         ; Contador PAINTCOLOR.
+        LD B, 4         ; Counter PAINTCOLOR.
 PAINTCOLOR:
-        LD (HL), C              ; Pintar cuadrado del color.
-        INC HL                  ; Siguiente cuadrado.
+        LD (HL), C              ; Paint color square.
+        INC HL                  ; Next square.
         DJNZ PAINTCOLOR         ; For (i = 4, i > 0, i--)
 CHANGECOLOR:
         LD A, C
-        ADD 8                   ; Cambiar color.
-        AND %00111000           ; Solo color paper.
+        ADD 8                   ; Change to next color.
+        AND %00111000           ; Keep just the paper color.
         LD C, A
         LD A, H
         CP $5B                  
