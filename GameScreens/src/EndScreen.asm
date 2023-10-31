7edc193ca69c45d32f526f0be465c5d11ff0ecb8
Carmen <carmen.moreno.udaondo@gmail.com>
2023-10-31 10:02:21 +0100

    Separate into different files

--- GameScreens/src/EndScreen.asm
+++ GameScreens/src/EndScreen.asm
@@ -1,4 +1,35 @@
+;-----------------------------------------------------------------------------------------
+; ENDINGSCREEN - Displays the end screen with its corresponding messages.
+;-----------------------------------------------------------------------------------------
+ENDINGSCREEN:
+        CALL CLEARSCR   ; Clean screen.
 
-ENDOFCODE:            
-        JR ENDOFCODE
+        ; Bye!
+        LD A, $42               ; Attribute - Red
+        LD B, 2                 ; Row
+        LD C, 2                 ; Column
+        LD IX, BYEMESSAGE       ; Bye!
+        CALL PRINTAT
+        
+        ; Play again? (Y/N)
+        LD A, $42               ; Attribute - Red
+        LD B, 4                 ; Row
+        LD C, 2                 ; Column
+        LD IX, PLAYAGAINMESSAGE ; Play again? (Y/N)
+        CALL PRINTAT
 
+        XOR A
+        CALL READYKEY
+        CP 1
+        JP Z, STARTINGSCREEN    ; Y - Start screen                        
+        LD B, 6                 ; N - End of code.
+        LD C, 2
+        LD IX, ENDMESSAGE       ; End!
+        CALL PRINTAT
+
+        JP ENDOFCODE
+;-----------------------------------------------------------------------------------------
+
+BYEMESSAGE: DB "BYE!", 0
+PLAYAGAINMESSAGE: DB "PLAY AGAIN? (Y/N)", 0
+ENDMESSAGE: DB "END!", 0
--- GameScreens/src/StartScreen.asm
+++ GameScreens/src/StartScreen.asm
@@ -2,9 +2,6 @@
 	SLDOPT COMMENT WPMEM, LOGPOINT, ASSERTION
         org $8000               ; Program located at $8000 = 32768.
 
-ASCII_Y equ $59
-ASCII_N equ $4E
-
 BEGIN:          
         DI              ; Disable interruptions.
         LD SP, 0        ; Set the stack pointer to the top of memory.
@@ -13,76 +10,38 @@ BEGIN:
 MAIN:
         CALL CLEARSCR   ; Clean screen.
 
+;-----------------------------------------------------------------------------------------
+; STARTINGSCREEN - Displays the start screen with its corresponding messages.
+;-----------------------------------------------------------------------------------------
 STARTINGSCREEN:
         ; Tetris!
-        LD A, $41       ; Attribute - Blue
-        LD B, 2         ; Row
-        LD C, 2         ; Column
-        LD IX, WELCOMEMESSAGE
+        LD A, $41               ; Attribute - Blue
+        LD B, 2                 ; Row
+        LD C, 2                 ; Column
+        LD IX, WELCOMEMESSAGE   ; Tetris!
         CALL PRINTAT
         
         ; Would you like to play? (y/n)
-        LD A, $41       ; Attribute - Blue
-        LD B, 4         ; Row
-        LD C, 2         ; Column
-        LD IX, PLAYMESSAGE
-        CALL PRINTAT
-        XOR A
-        CALL READYKEY
-        CP 1
-        JP Z, GAMESCREEN
-        JR ENDINGSCREEN
-
-ENDINGSCREEN:
-        CALL CLEARSCR   ; Clean screen.
-
-        ; Bye!
-        LD A, $42       ; Attribute - Red
-        LD B, 2         ; Row
-        LD C, 2         ; Column
-        LD IX, BYEMESSAGE
+        LD A, $41               ; Attribute - Blue
+        LD B, 4                 ; Row
+        LD C, 2                 ; Column
+        LD IX, PLAYMESSAGE      ; Would you like to play? (y/n)
         CALL PRINTAT
         
-        ; Would you like to play? (y/n)
-        LD A, $42       ; Attribute - Red
-        LD B, 4         ; Row
-        LD C, 2         ; Column
-        LD IX, PLAYAGAINMESSAGE
-        CALL PRINTAT
         XOR A
         CALL READYKEY
-        CP 1
-        JR Z, STARTINGSCREEN
-        LD B, 6         ; Row
-        LD C, 2         ; Column
-        LD IX, ENDMESSAGE
-        CALL PRINTAT
-        LD IX, ENDMESSAGE
-        CALL PRINTAT
+        CP 1            
+        JP Z, GAMESCREEN        ; Y - Game
+        JR ENDINGSCREEN         ; N - End screen
+;-----------------------------------------------------------------------------------------
+
+ENDOFCODE:            
         JR ENDOFCODE
 
-READYKEY:
-        LD A, $DF       ; Keys: Y, U, I, O, P
-        IN A, ($FE)     
-        BIT 4, A
-        JR NZ, READNKEY
-        LD A, 1
-        RET
-READNKEY:
-        LD A, $7F       ; Keys: B, N, M, SYMB, SPACE
-        IN A, ($FE)
-        BIT 3, A
-        JR NZ, READYKEY
-        LD A, 2
-        RET
-
 WELCOMEMESSAGE: DB "TETRIS!", 0
-PLAYMESSAGE: DB "WOULD YOU LIKE TO PLAY? (Y/N)", 0        ; 0 = delimitador de array.
-BYEMESSAGE: DB "BYE!", 0
-PLAYAGAINMESSAGE: DB "PLAY AGAIN? (Y/N)", 0        ; 0 = delimitador de array.
-ENDMESSAGE: DB "END!", 0
-
+PLAYMESSAGE: DB "WOULD YOU LIKE TO PLAY? (Y/N)", 0
 
         INCLUDE "GameScreen.asm"
         INCLUDE "EndScreen.asm"
+        INCLUDE "ReadKey.asm"
         INCLUDE "Printat.asm"
